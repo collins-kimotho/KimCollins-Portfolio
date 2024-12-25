da95ddbd586ce7c117d0d7fbea1b499d9e14c275
collins-kimotho <collinskimotho16@gmail.com>
2024-12-25 12:43:54 +0300

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Header from "./components/Header"
 const App = () => {
   return (
   <>
-    <Header />
+    <Header/>
   </>    
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,31 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0'>
+      <div className="max-w-screen-2xl w-full mx-auto px-4">
+        <h1>
+          <a href='/' className='logo'>
+            <img 
+              src="/images/logo.svg"
+              width={40}
+              height={40}
+               alt="Collins-Kimotho" />
+          </a>
+        </h1>
+        <div className=''>
+        <button
+          className=''
+          onClick={null}>
+          <span class="material-symbols-rounded">
+          menu
+          </span>        
+
+        </button>
+      </div>
+      </div>
+      
+
+    </header>
   )
 }
 
