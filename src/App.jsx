9ade5cfb234f2d16de7ced776dc3c98fd8fd9052
collins-kimotho <collinskimotho16@gmail.com>
2025-01-02 17:48:17 +0300

    start on my work section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import About from "./About"
 import Header from "./components/Header"
 import Hero from "./components/Hero"
 import Skill from "./components/Skill"
+import Work from "./components/Work"
 
 
 
@@ -13,6 +14,7 @@ const App = () => {
       <Hero />
       <About />
       <Skill />
+      <Work />
     </main>
   </>    
   )
