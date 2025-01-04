e8005d8a23c23dafc9706d92fb477015996f0928
collins-kimotho <collinskimotho16@gmail.com>
2025-01-04 19:36:21 +0300

    compress images

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,37 +3,37 @@ import ProjectCard from './ProjectCard';
 
 const works = [
   {
-    imgSrc: '/images/newsapp.png',
+    imgSrc: '/images/newsapp.jpg',
     title: 'DailyTimes: News App',
     tags: ['API', 'Web-design', 'Development'],
     projectLink: 'https://github.com/collins-kimotho/DailyTimes'
   },
   {
-    imgSrc: '/images/financeApp.png',
+    imgSrc: '/images/financeApp.jpg',
     title: 'Financial Advising Landing Page',
     tags: ['Web-design', 'React', 'Javascript'],
     projectLink: 'https://github.com/collins-kimotho/Finance-App'
   },
   {
-    imgSrc: '/images/scraping.webp',
+    imgSrc: '/images/scrapping.jpg',
     title: 'Web Scraping Projects',
     tags: ['Python', 'API', 'Web-scraping', 'Selenium'],
     projectLink: 'https://github.com/collins-kimotho/Scrapping-Data'
   },
   {
-    imgSrc: '/images/finsight.png',
+    imgSrc: '/images/finsight.jpg',
     title: 'Budgeting app landing page',
     tags: ['Web-design', 'Development', 'React'],
     projectLink: 'https://collins-kimotho.github.io/FinSight/'
   },
   {
-    imgSrc: '/images/portfolio.png',
+    imgSrc: '/images/portfolio.jpg',
     title: 'Personal portfolio',
     tags: ['React', 'Tailwind'],
     projectLink: 'https://github.com/collins-kimotho/KimCollins-Portfolio.git'
   },
   {
-    imgSrc: '/images/hbnb.png',
+    imgSrc: '/images/hbnb.jpg',
     title: 'AirBnb Clone',
     tags: ['Web-design', 'Development', 'Flask', 'Python'],
     projectLink: 'https://github.com/markdennis1287/AirBnB_clone_v3.git'
